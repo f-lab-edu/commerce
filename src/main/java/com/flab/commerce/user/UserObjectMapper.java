708245ec556b8760eb1aeb1059433cefcd5f7cd4
taekyun95 <jinhoa52@kakao.com>
2022-07-05 23:17:14 +0900

    #4 UserObjectMapper 오브젝트 매핑 설정 추가
    id => 무시
    createDate, updateDate => LocalDateTime.now()

--- src/main/java/com/flab/commerce/user/UserObjectMapper.java
+++ src/main/java/com/flab/commerce/user/UserObjectMapper.java
@@ -2,10 +2,15 @@ package com.flab.commerce.user;
 
 import com.flab.commerce.user.dto.RegisterDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserObjectMapper {
     UserObjectMapper INSTANCE = Mappers.getMapper(UserObjectMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createDate", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "updateDate", expression = "java(java.time.LocalDateTime.now())")
     User userToRegisterDto(RegisterDto registerDto);
 }
