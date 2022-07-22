2cecc1ba01205c1fe4721217af70e15e4df66065
peter-alethio <peter@alethio.io>
2022-07-22 13:23:47 +0900

    #8 비밀번호 확인 로직 제거
    프론트엔드에서 비밀번호 확인 처리

--- src/main/java/com/flab/commerce/domain/owner/OwnerService.java
+++ src/main/java/com/flab/commerce/domain/owner/OwnerService.java
@@ -10,10 +10,6 @@ public class OwnerService {
     private final OwnerMapper ownerMapper;
 
     public boolean register(OwnerRegisterDto registerRequest) {
-        if (!registerRequest.getPassword().equals(registerRequest.getPasswordConfirm())) {
-            throw new RuntimeException("비밀번호와 확인용 비밀번호가 일치하지 않습니다");
-        }
-
         if (ownerMapper.findByEmail(registerRequest.getEmail()) != null) {
             throw new RuntimeException("이미 존재하는 이메일입니다");
         }
